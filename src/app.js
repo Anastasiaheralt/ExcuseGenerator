fbe10b3bdc7398d455278a7b7d115c90857f1ca0
Anastasiaheralt <rosimarherrerao@gmail.com>
2022-07-11 03:49:10 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -2,10 +2,35 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//lets define our variables let with an array
+let who = ["El perro", "Mi abuela", "Mi tortuga", "Mi canario", "Mi gato"];
+let what = ["se comió", "aplastó", "robó", "rompió", "ensució"];
+let when = [
+  "antes de clases.",
+  "justo cuando terminé.",
+  "mientras almorzaba.",
+  "mientras yo rezaba."
+];
+
+function excuseGenerator(who, what, when) {
+  let numberWho = parseInt(Math.random() * who.length);
+  let numberWhat = parseInt(Math.random() * what.length);
+  let numberWhen = parseInt(Math.random() * when.length);
+
+  return (
+    who[numberWho] +
+    " " +
+    what[numberWhat] +
+    " " +
+    "mi tarea " +
+    when[numberWhen]
+  );
+}
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  var excusa = document.getElementById("excuse");
+  excusa.innerHTML = excuseGenerator(who, what, when);
 };
+
+window.onload = onload;
